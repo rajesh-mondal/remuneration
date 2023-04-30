b3b2533b92e572ca257766a5ba6b83e3d1014b48
rajesh-mondal <rajesh01749@gmail.com>
2023-04-30 21:46:23 +0600

    sidebar menu rearranged :art:

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -21,22 +21,23 @@
          <li><a href="{{ route('dashboard')}}"><i class="fa fa-dashboard yellow_color"></i> <span>Dashboard</span></a></li>
          <li><a href="{{ route('course.index')}}"><i class="fa fa-server green_color"></i> <span>Course</span></a></li>
          <li><a href="{{ route('teacher.index')}}"><i class="fa fa-user green_color"></i> <span>Teacher</span></a></li>
-         <li><a href="{{ route('exam.index')}}"><i class="fa fa-book green_color"></i> <span>Exam</span></a></li>
-         <li><a href="{{ route('remuneration.index')}}"><i class="fa fa-pencil red_color"></i> <span>Remuneration</span></a></li>
-         <li><a href="{{ route('remuneration-category.index')}}"><i class="fa fa-pencil red_color"></i> <span>Remuneration Category</span></a></li>
-         <li><a href="{{ route('remuneration-rate.index')}}"><i class="fa fa-pencil red_color"></i> <span>Remuneration Rate</span></a></li>
+         <li>
+            <a href="#element_rem" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-pencil-square-o green_color"></i> <span>Remuneration</span></a>
+            <ul class="collapse list-unstyled" id="element_rem">
+               <li><a href="{{ route('remuneration.index')}}"><i class="fa fa-pencil-square-o white_color"></i> <span>Remuneration</span></a></li>
+               <li><a href="{{ route('remuneration-category.index')}}"><i class="fa fa-folder-open white_color"></i> <span>Remuneration Category</span></a></li>
+               <li><a href="{{ route('remuneration-rate.index')}}"><i class="fa fa-dollar white_color"></i> <span>Remuneration Rate</span></a></li>
+            </ul>
+         </li>
          <li><a href="{{ route('discipline.index')}}"><i class="fa fa-building green_color"></i> <span>Descipline</span></a></li>
          <li><a href="{{ route('designation.index')}}"><i class="fa fa-building green_color"></i> <span>Designation</span></a></li>
-         <li><a href="{{ route('year.index')}}"><i class="fa fa-calendar green_color"></i> <span>Year</span></a></li>
-         <li><a href="{{ route('term.index')}}"><i class="fa fa-clock-o orange_color"></i> <span>Term</span></a></li>
-         <li><a href="{{ route('session.index')}}"><i class="fa fa-clock-o orange_color"></i> <span>Session</span></a></li>
          <li>
-            <a href="#element" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-diamond purple_color"></i> <span>Elements</span></a>
+            <a href="#element" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"><i class="fa fa-book green_color"></i> <span>Exam</span></a>
             <ul class="collapse list-unstyled" id="element">
                <li><a href="{{ route('exam.index')}}"><i class="fa fa-book white_color"></i><span>Exam</span></a></li>
                <li><a href="{{ route('year.index')}}"><i class="fa fa-calendar white_color"></i> <span>Year</span></a></li>
-               <li><a href="{{ route('term.index')}}"><i class="fa fa-clock-o orange_color"></i> <span>Term</span></a></li>
-               <li><a href="{{ route('session.index')}}"><i class="fa fa-clock-o orange_color"></i> <span>Session</span></a></li>
+               <li><a href="{{ route('term.index')}}"><i class="fa fa fa-clock-o white_color"></i> <span>Term</span></a></li>
+               <li><a href="{{ route('session.index')}}"><i class="fa fa fa-clock-o white_color"></i> <span>Session</span></a></li>
             </ul>
          </li>
          <li><a href="settings.html"><i class="fa fa-cog yellow_color"></i> <span>Settings</span></a></li>
